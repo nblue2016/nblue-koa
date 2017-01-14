0cf0a2ca84ef48bc5a1fc00d91018a97e8344c16
Yifeng Jiang <yifeng.jiang@mkcorp.com>
2017-01-14 14:07:15 +0800

    move app context to koa context

--- test/start.js
+++ test/start.js
@@ -1,8 +1,25 @@
+const nblue = require('nblue-core')
 const NKoa = require('../lib')
-const nkoa = new NKoa()
+const co = nblue.co
+
 const configFile = `${process.cwd()}/test/config.yml`
 
-nkoa.
+const nkoa = new NKoa()
+
+co(function *() {
+  yield nkoa.create(configFile)
+
+  // load defined middlewares
+  yield nkoa.use()
+
+  // appned defined routers
+  yield nkoa.routes()
+
+  // listen port to start web server
+  nkoa.listen()
+})
+
+/* nkoa.
   create(configFile).
   then(() => nkoa.use()).
   then(() => nkoa.routes()).
@@ -15,7 +32,7 @@ nkoa.
     if (logger) {
       logger.error(err.message)
     }
-  })
+  }) */
 
   /*
   co(function *() {
