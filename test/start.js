4c82fb82452dd469e93f3b591237602b3cab3bc6
Yifeng Jiang <yifeng.jiang@mkcorp.com>
2017-02-15 18:10:25 +0800

    working …

--- test/start.js
+++ test/start.js
@@ -1,21 +1,6 @@
-const core = require('nblue-core')
+// const core = require('nblue-core')
 const NKoa = require('../lib')
 
-const co = core.co
-
-const configFile = `${process.cwd()}/test/config.yml`
-
-const nkoa = new NKoa()
-
-co(function *() {
-  yield nkoa.create({ configFile })
-
-  // load defined middlewares
-  yield nkoa.use()
-
-  // appned defined routers
-  yield nkoa.routes()
-
-  // listen port to start web server
-  nkoa.listen()
+new NKoa().start({
+  configFile: `${process.cwd()}/test/config.yml`
 })
