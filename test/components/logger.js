f02f470e125f7b8e14072eddf886a6fd53e5b9cc
Yifeng Jiang <yifeng.jiang@mkcorp.com>
2017-03-02 18:19:02 +0800

    update

--- test/components/logger.js
+++ test/components/logger.js
@@ -1,50 +1,39 @@
-// const core = require('nblue-core')
-const SuperLogger = require('../../lib/components/logger')
+// reference libraries
+const core = require('nblue-core')
 
+// use classes
+const Component = require('../../lib/components/logger')
 const C = console
-const output = (line) => C.log(line)
-const HANDLER_KEY_OF_ERROR = 'error'
 
-class Logger extends SuperLogger {
+class Logger extends core.Logger {
 
-  koa () {
-    // assign this to that
-    const that = this
-
-    // get instance of logger
-    const logger = this.Logger
-
-    return function *(next) {
-      // get instance of context
-      const ctx = this
-
-      // get time for now
-      ctx.start = new Date()
-
-      yield next
-
-      const line = that.generateRequestLine(ctx)
-
-      // append info to logger
-      if (logger) {
-        logger.verbose(line)
-      }
-
-      output(line)
-    }
+  get Test () {
+    return 'OK'
   }
 
-  generateHandlers (logger) {
-    super.generateHandlers(logger)
+  log (level, message, options) {
+    // output error message to console
+    if (level === 1) {
+      C.log(message)
+      if (options) {
+        C.log(JSON.stringify(options, null, 2))
+      }
+    }
 
-    this._handlers.set(HANDLER_KEY_OF_ERROR, (err) => {
-      logger.error(`error(${err.name}) catched`, err)
-
-      output('error:')
-      output(err.message)
-    })
+    // call super method
+    super.log(level, message, options)
   }
 
 }
 
-module.exports = Logger
+
+class LoggerComponent extends Component {
+
+  createLoggerInstance (outputter) {
+    return new Logger(outputter)
+  }
+
+}
+
+
+module.exports = LoggerComponent
