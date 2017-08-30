5301a776c3879a2c592686450940fea741552f67
Yifeng Jiang <yifeng.jiang@mkcorp.com>
2017-08-30 17:26:01 +0800

    working...

--- test/controllers/controller1.js
+++ test/controllers/controller1.js
@@ -2,20 +2,32 @@ const Controller = require('../../lib/').Controller
 
 class Controller1 extends Controller {
 
-  method1 () {
-    return this.generateResponse({
-      a: 'm1',
-      b: 2,
-      c: 1
+  method1 (ctx) {
+    return this.respond(ctx, {
+      body: {
+        a: 'm1',
+        b: 2,
+        c: 1
+      }
     })
+
+    // return this.generateResponse()
   }
 
-  method2 () {
-    return this.generateResponse({
+  method2 (ctx) {
+    return this.respond(ctx, {
+      body: {
+        a: 'm1',
+        b: 2,
+        c: 1
+      }
+    })
+
+    /* return this.generateResponse({
       a: 'm2',
       b: 2,
       c: 1
-    })
+    }) */
   }
 
 }
--- test/controllers/test.js
+++ test/controllers/test.js
@@ -2,31 +2,20 @@ const Controller = require('../../lib/').Controller
 
 class TestRouter extends Controller {
 
-  ls () {
-    return this.generateResponse({
+  ls (ctx) {
+    this.respond(ctx, { body: {
       method: 'ls',
       args: [1, 2, 3]
-    })
+    } })
   }
 
-  // methods for koa
-  ktest () {
-    return function *() {
-      const ctx = this
-
-      ctx.body = { name: 'method1' }
-
-      yield Promise.resolve()
-    }
+  hello () {
+    // return this.generateResponse('O')
+    return null
   }
 
-  k2test (ctx) {
-    ctx.type = 'json'
-    ctx.body = { name: 'method1' }
-  }
-
-  etest (req, res) {
-    res.status(200).json({ name: 'method1' })
+  test (ctx) {
+    this.respond(ctx, { body: { name: 'method1' } })
   }
 
 }
